de8cbd3a1e78eb1ffb85812663d0fb810c2f68db
Andrew Jones <ajones@digitalbazaar.com>
2022-06-10 16:27:25 -0400

    Add logic for adding localhost implementations.

--- implementations/index.js
+++ implementations/index.js
@@ -2,9 +2,27 @@
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {createRequire} from 'node:module';
+import {join} from 'node:path';
+import appRoot from 'app-root-path';
+
 const require = createRequire(import.meta.url);
 const requireDir = require('require-dir');
 
 const dir = requireDir('./');
 
-export const implementerFiles = Object.values(dir);
+// gets local implementations from an optional config file
+const getLocalImplementations = () => {
+  try {
+    const path = join(
+      appRoot.toString(), '.vcApiTestImplementationsConfig.cjs');
+    return require(path);
+  } catch(e) {
+    if(e?.code === 'MODULE_NOT_FOUND') {
+      return [];
+    }
+    throw e;
+  }
+};
+
+export const implementerFiles = Object.values(dir)
+  .concat(getLocalImplementations());
